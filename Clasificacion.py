7334b727461eeb342facc4032e832dd2197f9502
Carlos Hernandez <117937022+CarlosAHzBt@users.noreply.github.com>
2024-01-15 23:53:23 -0700

    Add files via upload

---
+++ Clasificacion.py
@@ -0,0 +1,17 @@
+from ultralytics import YOLO
+import torch
+
+if torch.cuda.is_available():
+    print("CUDA está disponible. GPU:", torch.cuda.get_device_name(0))
+else:
+    print("CUDA no está disponible.")
+
+def train_model():
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model = YOLO("yolov8n-cls.pt")
+
+    model.train(data=r"C:\Users\carlo\PycharmProjects\PruebaSegmentacion\Imagenes\IMAGENES PARA MODELO v4\Organizado",
+                epochs=50, batch=16, imgsz=848)
+
+if __name__ == '__main__':
+    train_model()
