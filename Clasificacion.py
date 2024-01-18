14b661c0851ba28c3965650559e41c3da9e40672
Carlos Hernandez <117937022+CarlosAHzBt@users.noreply.github.com>
2024-01-17 20:34:21 -0700

    Add files via upload

--- Clasificacion.py
+++ Clasificacion.py
@@ -11,7 +11,11 @@ def train_model():
     model = YOLO("yolov8n-cls.pt")
 
     model.train(data=r"C:\Users\carlo\PycharmProjects\PruebaSegmentacion\Imagenes\IMAGENES PARA MODELO v4\Organizado",
-                epochs=50, batch=16, imgsz=848)
+                epochs=50, batch=8, imgsz=848)
+
+    torch.save(model.state_dict(), 'Modelo.pth')
+    torch.save(model, 'ModeloCompleto.pth')
+
 
 if __name__ == '__main__':
     train_model()
---
+++ ClasificacionV2.py
@@ -0,0 +1,38 @@
+from ultralytics import YOLO
+from PIL import Image
+import os
+
+# Load YOLO model
+model = YOLO("runs/classify/train9/weights/best.pt")
+
+# Paths to your image folders
+source_folder = 'Imagenes/IMAGENES PARA MODELO v4/Nada'
+target_folder_high_conf = 'BachesPrueba2'
+target_folder_low_conf = 'PrediccionesBajas'  # Carpeta para confianza baja
+
+# Ensure target folders exist
+os.makedirs(target_folder_high_conf, exist_ok=True)
+os.makedirs(target_folder_low_conf, exist_ok=True)
+
+# Iterate over each file in the folder
+for filename in os.listdir(source_folder):
+    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        image_path = os.path.join(source_folder, filename)
+        image = Image.open(image_path)
+
+        # Perform detection
+        results = model.predict(source=image, conf=0.5)
+        DeteccionGanadora = results[0].probs.top1
+        ConfianzaGanadora = results[0].probs.top1conf.item()
+
+        # Check if detections were found
+        if ConfianzaGanadora >= 0.5:
+            if DeteccionGanadora == 1:
+                image.save(os.path.join(target_folder_high_conf, filename))
+            elif DeteccionGanadora == 0:
+                image.save(os.path.join('CalleBien', filename))
+            elif DeteccionGanadora == 2:
+                image.save(os.path.join('Grietas', filename))
+        else:
+            # If the top prediction is below the confidence threshold, save to a separate folder
+            image.save(os.path.join('NoIdentificadas', filename))
