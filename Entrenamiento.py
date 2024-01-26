ab5a4efe0964c51a0c6f8d35ed8a1a1f5b079a2a
CarlosAHzBt <carlos.hb@itculiacan.edu.mx>
2024-01-26 14:28:28 -0600

    Holamundo

--- Entrenamiento.py
+++ Entrenamiento.py
@@ -8,7 +8,7 @@ else:
 
 def train_model():
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-    model = YOLO("yolov8n.pt")   #Cambiar entre yolov8n-cls.pt y yolov8n.pt dependiendo si es para clasificacion o deteccion
+    model = YOLO("yolov8n-cls.pt")   #Cambiar entre yolov8n-cls.pt y yolov8n.pt dependiendo si es para clasificacion o deteccion
 
     model.train(data=r"Imagenes/ImagenesModeloV2Segmentacion/Deteccion.v1i.yolov8/data.yaml",
                 epochs=500, batch=16, imgsz=848, patience=300)
