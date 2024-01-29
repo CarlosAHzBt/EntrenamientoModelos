5c59c07316c5d6d446337f70a4cd3c5bd5254e41
CarlosAHzBt <carlos.hb@culiacan.tecnm.mx>
2024-01-28 22:40:35 -0700

    Entrenamiento de deteccion v3

--- DeteccionV1.py
+++ DeteccionV1.py
@@ -4,54 +4,66 @@ from ultralytics.utils.plotting import Annotator
 import os
 
 # Carga el modelo YOLO
-model = YOLO("runs/detect/train5/weights/best.pt")
+model = YOLO(r'C:\Users\carlo\PycharmProjects\pythonProject4\EntrenamientoModelos\Modelos\Deteccion\ModeloV3\weights\best.pt')
 
-# Rutas a tus carpetas de imágenes
-source_folder = 'Imagenes/IMAGENES PARA MODELO v4/Bache'
-target_folder = 'ImagenesDeDeteccionV1/ImagenesConBachesDetectados'
+# Rutas a tu carpeta raíz de imágenes
+root_folder = r'E:\Imagenes\Imagenes - copia'
 
-# Asegúrate de que exista la carpeta de destino
-os.makedirs(target_folder, exist_ok=True)
+# Asegúrate de que exista la carpeta de destino para imágenes y txt
+target_folder_images = r'E:\Resultados Por Versiones De los modelos, Imagenes\ImgenesDeteccionPorModelos\ModeloV3\Bache'
+target_folder_txt = r'E:\Resultados Por Versiones De los modelos, Imagenes\ImgenesDeteccionPorModelos\ModeloV3\Coordenadas'
+target_folder_no_identificadas = r'E:\Resultados Por Versiones De los modelos, Imagenes\ImgenesDeteccionPorModelos\ModeloV3\NoIdentificadas'
 
-# Itera sobre cada archivo en la carpeta
-for filename in os.listdir(source_folder):
-    if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-        image_path = os.path.join(source_folder, filename)
-        frame = cv2.imread(image_path)
-        img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+os.makedirs(target_folder_images, exist_ok=True)
+os.makedirs(target_folder_txt, exist_ok=True)
+os.makedirs(target_folder_no_identificadas, exist_ok=True)
 
-        # Realiza la detección
-        results = model.predict(img)
-        names = model.names
+# Itera sobre todas las carpetas y archivos en la carpeta raíz
+for foldername, subfolders, filenames in os.walk(root_folder):
+    for filename in filenames:
+        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+            image_path = os.path.join(foldername, filename)
+            frame = cv2.imread(image_path)
+            img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-        car_id = list(names)[list(names.values()).index('Bache')]
-        number = results[0].boxes.cls.tolist().count(car_id)
-        try:
-            detections = results[0].boxes.xyxy[0]  # Get detection bounding boxes
-        except:
-            detections = 0
-        annotator = Annotator(frame, line_width=2, font_size=10)
-        if number >= 1:
-            for r in results:
-                boxes = r.boxes
-                for box in boxes:
-                    b = box.xyxy[0]  # get box coordinates
-                    c = box.cls
-                    annotator.box_label(b, model.names[int(c)])
+            # Realiza la detección
+            results = model.predict(img)
+            names = model.names
 
-            # Guarda la imagen con detecciones
-            result_frame = annotator.result()
-            cv2.imwrite(os.path.join(target_folder, filename), result_frame)
-            # Guardar el txt con las coordenadas de los baches
-            # Save coordinates to a txt file
-            with open(os.path.join("ImagenesDeDeteccionV1/detected_coords", filename.replace('.png', '.txt')), 'w') as f:
-                f.write(str(detections.tolist()))
+            car_id = list(names)[list(names.values()).index('Bache')]
+            number = results[0].boxes.cls.tolist().count(car_id)
+            try:
+                detections = results[0].boxes.xyxy[0]  # Get detection bounding boxes
+            except:
+                detections = 0
+            annotator = Annotator(frame, line_width=2, font_size=10)
+            if number >= 1:
+                for r in results:
+                    boxes = r.boxes
+                    for box in boxes:
+                        b = box.xyxy[0]  # get box coordinates
+                        c = box.cls
+                        annotator.box_label(b, model.names[int(c)])
 
+                # Guarda la imagen con detecciones
+                result_frame = annotator.result()
+                output_folder_images = os.path.join(target_folder_images, os.path.relpath(foldername, root_folder))
+                os.makedirs(output_folder_images, exist_ok=True)
+                output_path_images = os.path.join(output_folder_images, filename)
+                cv2.imwrite(output_path_images, result_frame)
 
+                # Guardar el txt con las coordenadas de los baches
+                output_folder_txt = os.path.join(target_folder_txt, os.path.relpath(foldername, root_folder))
+                os.makedirs(output_folder_txt, exist_ok=True)
+                output_path_txt = os.path.join(output_folder_txt, filename.replace('.png', '.txt'))
+                with open(output_path_txt, 'w') as f:
+                    f.write(str(detections.tolist()))
 
-        else :
-            cv2.imwrite(os.path.join("ImagenesDeDeteccionV1/NoIdentificadas", filename), frame)
-
+            else:
+                output_folder_no_identificadas = os.path.join(target_folder_no_identificadas, os.path.relpath(foldername, root_folder))
+                os.makedirs(output_folder_no_identificadas, exist_ok=True)
+                output_path_no_identificadas = os.path.join(output_folder_no_identificadas, filename)
+                cv2.imwrite(output_path_no_identificadas, frame)
 
 # Mensaje al finalizar el procesamiento
-print("Procesamiento completo, imágenes guardadas en:", target_folder)
+print("Procesamiento completo, imágenes y txt guardados en:", target_folder_images, target_folder_txt, target_folder_no_identificadas)
