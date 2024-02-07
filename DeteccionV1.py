b409a6cd4152b683e9748746a0ba40a39443d21a
CarlosAHzBt <carlos.hb@culiacan.tecnm.mx>
2024-02-06 22:35:18 -0700

    Mejoras

--- DeteccionV1.py
+++ DeteccionV1.py
@@ -4,21 +4,18 @@ from ultralytics.utils.plotting import Annotator
 import os
 
 # Carga el modelo YOLO
-model = YOLO(r'C:\Users\carlo\PycharmProjects\pythonProject4\EntrenamientoModelos\Modelos\Deteccion\ModeloV3\weights\best.pt')
+model = YOLO(r"SegmentacionV1\train5\weights\best.pt")
 
-# Rutas a tu carpeta raíz de imágenes
-root_folder = r'E:\Imagenes\Imagenes - copia'
+# Rutas a tus carpetas de imágenes
+source_folder = r'E:\Imagenes\6FebLey\20240206_073721'
+target_folder = r'Imagenes\ImagenesResultadosDeDeteccion\ImagenesConBachesDetectados'
+detected_coords_folder = r'Imagenes\ImagenesResultadosDeDeteccion\detected_coords'
+no_identificadas_folder = r'Imagenes\ImagenesResultadosDeDeteccion\NoIdentificadas'
 
-# Asegúrate de que exista la carpeta de destino para imágenes y txt
-target_folder_images = r'E:\Resultados Por Versiones De los modelos, Imagenes\ImgenesDeteccionPorModelos\ModeloV4\Bache'
-target_folder_images_without_bounding_box = r'E:\Resultados Por Versiones De los modelos, Imagenes\ImgenesDeteccionPorModelos\ModeloV4\BacheSinBBox'
-target_folder_txt = r'E:\Resultados Por Versiones De los modelos, Imagenes\ImgenesDeteccionPorModelos\ModeloV4\Coordenadas'
-target_folder_no_identificadas = r'E:\Resultados Por Versiones De los modelos, Imagenes\ImgenesDeteccionPorModelos\ModeloV4\NoIdentificadas'
-
-os.makedirs(target_folder_images, exist_ok=True)
-os.makedirs(target_folder_images_without_bounding_box, exist_ok=True)
-os.makedirs(target_folder_txt, exist_ok=True)
-os.makedirs(target_folder_no_identificadas, exist_ok=True)
+# Asegúrate de que existan las carpetas de destino
+os.makedirs(target_folder, exist_ok=True)
+os.makedirs(detected_coords_folder, exist_ok=True)
+os.makedirs(no_identificadas_folder, exist_ok=True)
 
 # Itera sobre todas las carpetas y archivos en la carpeta raíz
 for foldername, subfolders, filenames in os.walk(root_folder):
@@ -28,54 +25,28 @@ for foldername, subfolders, filenames in os.walk(root_folder):
             frame = cv2.imread(image_path)
             img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
-            # Realiza la detección
-            results = model.predict(img)
-            names = model.names
+        # Realiza la detección
+        results = model.predict(img)
+        with open(os.path.join(detected_coords_folder, filename.replace('.png', '.txt')), 'a') as f:
+            for result in results:
+                if results[0].boxes is not None and len(results[0].boxes) > 0:  # Verifica si hay detecciones
+                    # hacer algo con la detección           
+                    annotator = Annotator(frame, line_width=2, font_size=10)
+                    for det in result.boxes:  # Itera sobre cada detección
+                        b = det.xyxy[0] # Coordenadas de la caja
+                        c = int(det.cls)  # Clase de la detección
+                        annotator.box_label(b, model.names[c])
 
-            car_id = list(names)[list(names.values()).index('Bache')]
-            number = results[0].boxes.cls.tolist().count(car_id)
-            try:
-                detections = results[0].boxes.xyxy[0]  # Get detection bounding boxes
-            except:
-                detections = 0
-            annotator = Annotator(frame, line_width=4, font_size=10)
-            if number >= 1:
-                # Guardar la imagen sin detecciones
-                output_folder_images_without_bounding_box = os.path.join(target_folder_images_without_bounding_box,
-                                                                         os.path.relpath(foldername, root_folder))
-                os.makedirs(output_folder_images_without_bounding_box, exist_ok=True)
-                output_path_images_without_bounding_box = os.path.join(output_folder_images_without_bounding_box,
-                                                                       filename)
-                cv2.imwrite(output_path_images_without_bounding_box, frame)
-                for r in results:
-                    boxes = r.boxes
-                    for box in boxes:
-                        b = box.xyxy[0]  # get box coordinates
-                        c = box.cls
-                        annotator.box_label(b, model.names[int(c)])
+                        # Guarda las coordenadas de los baches detectados en un archivo txt
+                        #with open(os.path.join(detected_coords_folder, filename.replace('.png', '.txt')), 'a') as f:
+                        f.write(f"{b[0]}, {b[1]}, {b[2]}, {b[3]}\n")
 
-                # Guarda la imagen con detecciones
-                result_frame = annotator.result()
-                output_folder_images = os.path.join(target_folder_images, os.path.relpath(foldername, root_folder))
-                os.makedirs(output_folder_images, exist_ok=True)
-                output_path_images = os.path.join(output_folder_images, filename)
-                cv2.imwrite(output_path_images, result_frame)
-
-
-
-
-                # Guardar el txt con las coordenadas de los baches
-                output_folder_txt = os.path.join(target_folder_txt, os.path.relpath(foldername, root_folder))
-                os.makedirs(output_folder_txt, exist_ok=True)
-                output_path_txt = os.path.join(output_folder_txt, filename.replace('.png', '.txt'))
-                with open(output_path_txt, 'w') as f:
-                    f.write(str(detections.tolist()))
-
-            else:
-                output_folder_no_identificadas = os.path.join(target_folder_no_identificadas, os.path.relpath(foldername, root_folder))
-                os.makedirs(output_folder_no_identificadas, exist_ok=True)
-                output_path_no_identificadas = os.path.join(output_folder_no_identificadas, filename)
-                cv2.imwrite(output_path_no_identificadas, frame)
+                    # Guarda la imagen con detecciones
+                    result_frame = annotator.result()
+                    cv2.imwrite(os.path.join(target_folder, filename), result_frame)
+                else:
+                    # Guarda imágenes sin detecciones identificadas en una carpeta específica
+                    cv2.imwrite(os.path.join(no_identificadas_folder, filename), frame)
 
 # Mensaje al finalizar el procesamiento
 print("Procesamiento completo, imágenes y txt guardados en:", target_folder_images, target_folder_txt, target_folder_no_identificadas)
